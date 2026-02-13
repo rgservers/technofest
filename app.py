89b24e967b6ebe479a9bb0af6066192c7ee4c500
Rishi Gandhi <rishi@rgserver.in>
2026-02-13 12:34:20 +0530

    V2

--- app.py
+++ app.py
@@ -16,7 +16,19 @@ myclient = pymongo.MongoClient('mongodb://localhost:27017/')
 
 mydb = myclient['mydatabase']
 
+col1 = mydb['mycollection']
+
 app = flask.Flask(__name__)
 @app.route('/', methods=['GET'])
 def home():
     return flask.send_file('templates/index.html')
+
+@app.route('/search', methods=['GET'])
+def search():
+    query = flask.request.args.get('query')
+    # get the query embedding
+    # search in elasticsearch
+    # return the results
+    col1.insert_one({"query": query, "timestamp":"hi" })
+    return "inserted"
+
