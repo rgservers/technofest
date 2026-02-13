502326eef54360bb947d5f8a82c188e3233f2343
Sahishnu-Mac <sahi7777smart@gmail.com>
2026-02-13 15:47:26 +0530

    Embeddings task

--- embeddings_manager.py
+++ embeddings_manager.py
@@ -1,11 +1,29 @@
-import pymongo
-import elasticsearch
+import pymongo 
 import flask
 import numpy as np
 from sentence_transformers import SentenceTransformer, util
 import pandas as pd
+import os
+import json
+
+client = pymongo.MongoClient('mongodb://localhost:27017/')
+mydb = client['VadaPavFashion']
+collection = mydb['fashion_items']
+
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
+data = pd.read_csv('fashon-dataset/styles.csv')
+
+for index, row in data.iterrows():
+    id = row['id']
+    if os.path.exists:
+        try:
+            vector_text = model.encode(row['productDisplayName'])
+            collection.find_one_and_update({"id":id}, {'text_vector': vector_text})
+            print(f'{id} is done')
+        except:
+            print('error')
+
 
 
 
