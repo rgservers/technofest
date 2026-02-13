d0437a447a938e16c6161c02de659105d6f7becc
Sahishnu-Mac <sahi7777smart@gmail.com>
2026-02-13 18:12:09 +0530

    Audit 2

--- test.py
+++ test.py
@@ -1,22 +1,18 @@
 import pymongo
 
-# 1. Establish connection and select collection
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["VadaPavFashion"]
 mycol = mydb["fashion_items"]
 
-# 2. Use find() with projection to get only the 'name' field
-# {} is the query (empty means all documents)
-# {"name": 1, "_id": 0} is the projection (include 'name', exclude '_id')
+
 projection = {
     "id": 1, 
     "vector": 1, 
     "_id": 0
 }
 
-# 2. Execute the find
+
 results = mycol.find({}, projection)
-# 3. Print the count of documents
 print(f"Estimated document count: {results}")
 
 for doc in results:
--- vector_organizer.py
+++ vector_organizer.py
@@ -1,25 +1,49 @@
 import pymongo 
 from sentence_transformers import SentenceTransformer, util
-import json
+
+def find_relevant(data):
+    myclient = pymongo.MongoClient('mongodb://localhost:27017/')
+    mydb = myclient['VadaPavFashion']
+    col1 = mydb['fashion_items']
+
+    vector_data = []
+
+    projection = {
+        "id": 1, 
+        "vector": 1, 
+        "_id": 0
+    }
+    results = col1.find({}, projection)
+    for doc in results:
+        product_id = doc.get("id")
+        vector_data = doc.get("vector")
+
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+
+    embed_1 = model.encode('text')
+    result = {}
+
+    for key, value in vector_data:
+        embed_2 = data
+        result['key'] = (util.cos_sim(embed_1, embed_2))
+
+    return {k: v for k, v in sorted(result.items())}
+
 
 myclient = pymongo.MongoClient('mongodb://localhost:27017/')
 mydb = myclient['VadaPavFashion']
 col1 = mydb['fashion_items']
+projection = {
+    "id": 1, 
+    "vector": 1, 
+    "_id": 0
+}
+results = col1.find({}, projection)
+for doc in results:
+    product_id = doc.get("id")
+    vector_data = doc.get("vector")
 
-def find_relevant():
-    users_collection = mydb['users']
+result = find_relevant("Stylish men's shirt")
+print(result)
 
-    vec1 = col1.find()
-
-    data = json.loads(str(vec1))
-    model = SentenceTransformer("all-MiniLM-L6-v2")
-
-    embed_1 = model.encode('text')
-    result = []
-
-    for key, value in data.items():
-        embed_2 = []
-        result.append(util.cos_sim(embed_1, embed_2))
-
-    result.sort(reverse=True)
 
