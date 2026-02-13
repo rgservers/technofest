ec331242b5e15889e8b50b195b9ff1dec7adb3be
Rishi Gandhi <rishi@rgserver.in>
2026-02-13 10:38:18 +0530

    db connectors added

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 import pymongo
 import elasticsearch
 
+mainclient = pymongo.MongoClient("mongodb://localhost:27017/")
+maindb = mainclient["VadaPavFassion"]
+
+collection1 = maindb.create_collection["sureshites"]
