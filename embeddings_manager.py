b12179903a2dda9cc2b312d7f43b7a0e9e51e570
Rishi Gandhi <rishi@rgserver.in>
2026-02-13 16:06:50 +0530

    embeddings update

--- embeddings_manager.py
+++ embeddings_manager.py
@@ -12,17 +12,34 @@ collection = mydb['fashion_items']
 
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
-data = pd.read_csv('fashon-dataset/styles.csv')
+data = pd.read_csv('fashion-dataset/styles.csv', on_bad_lines='skip')
+
+# Columns to vectorize
+dse = 0
 
 for index, row in data.iterrows():
     id = row['id']
-    if os.path.exists:
-        try:
-            vector_text = model.encode(row['productDisplayName'])
-            collection.find_one_and_update({"id":id}, {'text_vector': vector_text})
-            print(f'{id} is done')
-        except:
-            print('error')
+    try:
+        update_dict = {}
+        text_total = ''
+        for col in ['gender', 'masterCategory', 'subCategory', 'articleType', 'baseColour', 'season', 'usage', 'productDisplayName']:
+            if pd.isna(row[col]):
+                print(f'Skipping {col} for {id}: {col} is NaN')
+                continue
+            text_total += row[col] + ' '
+        vector = model.encode(text_total)
+        update_dict['main_vector'] = vector.tolist()
+        if update_dict:
+            result = collection.find_one_and_update({"id": id}, {'$set': update_dict})
+            if result:
+                dse += 1
+                print(f'{id} vectors updated, documents processed: {dse}')
+            else:
+                print(f'{id} not found in database')
+        else:
+            print(f'{id} no vectors to update')
+    except Exception as e:
+        print(f'Error processing {id}: {e}')
 
 
 
