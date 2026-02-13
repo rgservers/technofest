7da16101a4e112a7b326e961eab4816f095234c2
Rishi Gandhi <rishi@rgserver.in>
2026-02-13 11:14:44 +0530

    IMP

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import pymongo
-import elasticsearch
+#import elasticsearch
 import flask
-import numpy as np
+#import numpy as np
 
-mainclient = pymongo.MongoClient("mongodb://localhost:27017/")
-maindb = mainclient["VadaPavFassion"]
+mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
+mongo_db = mongo_client["VadaPavFassion"]
+mongo_collection = mongo_db["sureshites"]
 
-collection1 = maindb.create_collection["sureshites"]
+app = flask.Flask(__name__)
+@app.route('/', methods=['GET'])
+def home():
+    return "Welcome to VadaPavFassion API"
